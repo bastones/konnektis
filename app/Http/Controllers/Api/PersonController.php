0591661ad3a5e882c71499bd1397f2b0c41f92a1
Ben Stones <ben@b3ns.com>
2018-08-08 15:34:00 +0100

    Add support for adding new person records

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\StorePersonRequest;
 use App\Http\Resources\PersonResource;
 use App\Models\Person;
 use Illuminate\Http\Request;
@@ -16,24 +17,38 @@ class PersonController extends Controller
      */
     public function index()
     {
-        return PersonResource::collection(Person::paginate(10));
+        return PersonResource::collection(Person::orderBy('created_at', 'desc')->paginate(10));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param StorePersonRequest|Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePersonRequest $request)
     {
-        //
+        Person::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'phone' => $request->get('phone'),
+        ]);
+
+        if ($request->expectsJson()) {
+            return response()->json([
+                'success' => true,
+            ]);
+        }
+
+        return redirect()->back()->with([
+            'success' => true,
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -44,8 +59,8 @@ class PersonController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -56,7 +71,7 @@ class PersonController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -4,39 +4,102 @@
     </div>
 
     <div v-else class="container pt-4 pb-4">
-        <table class="table table-striped table-responsive-sm">
-            <thead>
-                <tr>
-                    <td>
-                        Full Name
-                    </td>
+        <form @submit.prevent="submitForm" novalidate>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <td>
+                                Full Name
+                            </td>
 
-                    <td>
-                        Email Address
-                    </td>
+                            <td>
+                                Email Address
+                            </td>
 
-                    <td>
-                        Phone Number
-                    </td>
-                </tr>
-            </thead>
+                            <td>
+                                Phone No.
 
-            <tbody>
-                <template v-for="person in people">
-                    <tr>
-                        <td v-text="person.name"></td>
+                                <a v-if="! form.show" href="javascript:;" class="text-secondary" @click="showForm">
+                                    <i class="fas fa-plus-circle"></i>
+                                </a>
 
-                        <td v-text="person.email"></td>
+                                <a v-else href="javascript:;" class="text-danger" @click="hideForm">
+                                    <i class="fas fa-minus-circle"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </thead>
 
-                        <td v-text="person.phone"></td>
-                    </tr>
-                </template>
-            </tbody>
-        </table>
+                    <tbody>
+                        <tr v-if="form.show">
+                            <td>
+                                <input type="text"
+                                       class="form-control"
+                                       :class="{ 'is-invalid': form.errors && typeof form.errors.name !== typeof undefined }"
+                                       @keydown="removeError('name')"
+                                       placeholder="What's the full name?"
+                                       v-model="form.data.name"
+                                       ref="name">
+
+                                <div class="invalid-feedback"
+                                     v-if="form.errors && typeof form.errors.name !== typeof undefined"
+                                     v-text="form.errors.name[0]"></div>
+                            </td>
+
+                            <td>
+                                <input type="email"
+                                       class="form-control"
+                                       :class="{ 'is-invalid': form.errors && typeof form.errors.email !== typeof undefined }"
+                                       @keydown="removeError('email')"
+                                       placeholder="What's the email address?"
+                                       v-model="form.data.email">
+
+                                <div class="invalid-feedback"
+                                     v-if="form.errors && typeof form.errors.email !== typeof undefined"
+                                     v-text="form.errors.email[0]"></div>
+                            </td>
+
+                            <td class="d-flex flex-row">
+                                <div class="w-100 mr-3">
+                                    <input type="text"
+                                           class="form-control"
+                                           :class="{ 'is-invalid': form.errors && typeof form.errors.phone !== typeof undefined }"
+                                           @keydown="removeError('phone')"
+                                           placeholder="What's the phone number?"
+                                           v-model="form.data.phone">
+
+
+                                    <div class="invalid-feedback"
+                                         v-if="form.errors && typeof form.errors.phone !== typeof undefined"
+                                         v-text="form.errors.phone[0]"></div>
+                                </div>
+
+                                <div class="d-inline-block">
+                                    <button class="btn btn-secondary" :disabled="refreshing">
+                                        Add
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <template v-for="person in people">
+                            <tr>
+                                <td v-text="person.name"></td>
+
+                                <td v-text="person.email"></td>
+
+                                <td v-text="person.phone"></td>
+                            </tr>
+                        </template>
+                    </tbody>
+                </table>
+            </div>
+        </form>
 
         <nav>
             <paginate
-                    v-model="pageNumber"
+                    v-model="page"
                     :page-count="meta.last_page"
                     :page-range="3"
                     :margin-pages="2"
@@ -45,6 +108,8 @@
                     :next-text="'Next'"
                     container-class="pagination justify-content-center mt-4"
                     page-class="page-item"
+                    disabled-class="page-item disabled"
+                    next-class="page-item"
                     prev-link-class="page-link"
                     next-link-class="page-link"
                     page-link-class="page-link">
@@ -63,7 +128,19 @@
             return {
                 people: [],
                 meta: [],
-                pageNumber: 1,
+                page: 1,
+
+                form: {
+                    show: false,
+
+                    errors: null,
+
+                    data: {
+                        name: '',
+                        email: '',
+                        phone: '',
+                    },
+                },
 
                 loading: true,
                 refreshing: false,
@@ -72,7 +149,7 @@
 
         methods: {
             getPeople() {
-                axios.get('/api/people?page=' + this.pageNumber).then(HttpResponse => {
+                axios.get('/api/people?page=' + this.page).then(HttpResponse => {
                     let data = HttpResponse.data;
 
                     this.people = data.data;
@@ -81,7 +158,8 @@
                     this.loading = false;
                     this.refreshing = false;
 
-                    console.log(data);
+                    this.form.show = false;
+                    this.emptyForm();
                 });
             },
 
@@ -89,6 +167,53 @@
                 this.refreshing = true;
 
                 this.getPeople();
+            },
+
+            showForm() {
+                this.form.show = true;
+
+                this.$nextTick(() => {
+                    this.$refs.name.focus();
+                });
+            },
+
+            hideForm() {
+                this.form.show = false;
+
+                this.emptyForm();
+            },
+
+            emptyForm() {
+                this.form.errors = null;
+                this.form.data.name = '';
+                this.form.data.email = '';
+                this.form.data.phone = '';
+            },
+
+            submitForm() {
+                this.refreshing = true;
+
+                axios.post('/api/people', this.form.data).then(HttpResponse => {
+                    if (HttpResponse.data.success) {
+                        this.getPeople();
+                    }
+                }).catch(HttpResponse => {
+                    if (typeof HttpResponse.response.data.errors !== typeof undefined) {
+                        this.form.errors = HttpResponse.response.data.errors;
+                    }
+
+                    this.refreshing = false;
+
+                    this.$nextTick(() => {
+                        this.$refs.name.focus();
+                    });
+                });
+            },
+
+            removeError(key) {
+                if (this.form.errors && typeof this.form.errors[key] !== typeof undefined) {
+                    delete this.form.errors[key];
+                }
             },
         },
 
@@ -103,3 +228,24 @@
         },
     };
 </script>
+
+<style scoped>
+    @media (max-width: 992px) {
+        .table td,
+        .table td > div:first-child {
+            min-width: 250px;
+        }
+    }
+
+    .table > thead > tr > td {
+        position: relative;
+    }
+
+    .table > thead > tr > td > a {
+        position: absolute;
+        top: 6px;
+        right: 2px;
+        font-size: 18px;
+        padding: 5px 8px;
+    }
+</style>
